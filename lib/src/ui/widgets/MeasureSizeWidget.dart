2aa301c324cea21258d5dd25a82d7846381430d1
non.auth0 <truecollaboration0@gmail.com>
2022-05-05 22:14:34 +0200

    0.0.2: beta

--- lib/src/ui/widgets/MeasureSizeWidget.dart
+++ lib/src/ui/widgets/MeasureSizeWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:true_core_flutter/library.dart';
 
 typedef void OnWidgetSizeChangeFunction(Size size);
 
