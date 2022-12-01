94c32c7c34e86908dc1c0f861ff9b2d59485e69d
non.auth0 <truecollaboration0@gmail.com>
2022-12-01 21:17:46 +0100

    some fixes

--- lib/src/features/Notifier/extensions/ListenableExtension.dart
+++ lib/src/features/Notifier/extensions/ListenableExtension.dart
@@ -6,7 +6,7 @@ extension ListenableExtension<T> on ValueListenable<T> {
     final listener = this;
     final func = () => callback(listener.value);
     listener.addListener(func);
-    storage.add(_NotifierSubscriptionListenable<T>(
+    storage.add(_ListenableSubscription<T>(
       listener: listener,
       callback: callback,
       function: func,
@@ -14,11 +14,36 @@ extension ListenableExtension<T> on ValueListenable<T> {
   }
 }
 
-class _NotifierSubscriptionListenable<T> implements NotifierSubscription<T> {
+extension StorageExtension on NotifierStorage {
+  void addChangeNotifier<T extends ChangeNotifier>(T listener, NotifierCallback<T> callback) {
+    final func = () => callback(listener);
+    listener.addListener(func);
+    add(_ChangeNotifierSubscription<T>(
+      listener: listener,
+      callback: callback,
+      function: func,
+    ));
+  }
+}
+
+// extension ChangeNotifierExtension on ChangeNotifier {
+//   void addTo<T extends ChangeNotifier>(NotifierStorage storage, NotifierCallback<T> callback) {
+//     final listener = this as T;
+//     final func = () => callback(listener);
+//     listener.addListener(func);
+//     storage.add(_ChangeNotifierSubscription<T>(
+//       listener: listener,
+//       callback: callback,
+//       function: func,
+//     ));
+//   }
+// }
+
+class _ListenableSubscription<T> implements NotifierSubscription<T> {
   final ValueListenable<T> listener;
   final NotifierCallback<T> callback;
   VoidCallback? function;
-  _NotifierSubscriptionListenable({
+  _ListenableSubscription({
     required this.listener,
     required this.callback,
     required this.function,
@@ -35,6 +60,39 @@ class _NotifierSubscriptionListenable<T> implements NotifierSubscription<T> {
   @override
   NotifierSubscription<T> execute() {
     callback(listener.value);
+    return this;
+  }
+
+  @override
+  void cancel() {
+    if(function != null) {
+      listener.removeListener(function!);
+      function = null;
+    }
+  }
+}
+
+class _ChangeNotifierSubscription<T extends ChangeNotifier> implements NotifierSubscription<T> {
+  final T listener;
+  final NotifierCallback<T> callback;
+  VoidCallback? function;
+  _ChangeNotifierSubscription({
+    required this.listener,
+    required this.callback,
+    required this.function,
+  });
+
+  @override
+  NotifierSubscription<T> addTo(NotifierStorage storage) {
+    final func = () => callback(listener);
+    listener.addListener(func);
+    storage.add(this);
+    return this;    
+  }
+
+  @override
+  NotifierSubscription<T> execute() {
+    callback(listener);
     return this;
   }
 
